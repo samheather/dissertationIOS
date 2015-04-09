3f2eef99dcfcacbd869bf36eb4e0d18e26149416
Sam Heather <sam@heather.sh>
2015-04-09 14:22:25 +0100

    Initial commit, empty project

---
+++ Dissertation/Dissertation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Dissertation
+//
+//  Created by Samuel B Heather on 09/04/2015.
+//  Copyright (c) 2015 Samuel B Heather. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Dissertation/Dissertation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dissertation
+//
+//  Created by Samuel B Heather on 09/04/2015.
+//  Copyright (c) 2015 Samuel B Heather. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
